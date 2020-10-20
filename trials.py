01cbd9604ddba0278df93c20dc8120cb3c345258
Ashley Kwan <ashleykwan888@gmail.com>
2020-10-20 20:06:04 +0000

    completed 7 trials

--- trials.py
+++ trials.py
@@ -2,32 +2,80 @@
 
 
 def output_all_items(items):
-    pass  # TODO: replace this line with your code
+    # TODO: replace this line with your code
+    for item in items:
+        print(item)
 
+output_all_items([1, 'hello', 'true'])
 
 def get_all_evens(nums):
-    pass  # TODO: replace this line with your code
-
+    # TODO: replace this line with your code
+    even_nums = []
+    for num in nums:
+        if num % 2 == 0:
+            even_nums.append(num)
+    
+    return even_nums
+print(get_all_evens([7,8,10,1,2,2]))
 
 def get_odd_indices(items):
-    pass  # TODO: replace this line with your code
+    # TODO: replace this line with your code
+    results = []
+
+    for i in range(len(items)):
+
+        if i % 2 != 0:
+            results.append(item[i])
+
+    return results
+
+print(get_odd_indices([1, 'hello', true, 500]))
 
 
 def print_as_numbered_list(items):
-    pass  # TODO: replace this line with your code
+    i = 1
+    for item in items:
+        print(f"{i}.{item} ") 
+        i +=1 
 
+print_as_numbered_list([1,"hello", true])
 
 def get_range(start, stop):
-    pass  # TODO: replace this line with your code
+    # TODO: replace this line with your code
+    nums = []
+    num = start
 
+    while True:
+        if num < stop:
+            num += 1
+            nums.append(num)
+    
+    return nums
+            
+print(get_range(0, 5))
 
 def censor_vowels(word):
-    pass  # TODO: replace this line with your code
+    chars = []
+    for letter in word:
+        if 'aeiou' in letter:
+            chars.append('*')
+        else:
+            chars.append(letter)
+    return "".join(chars)
 
+print(censor_vowels("Hello World"))
 
 def snake_to_camel(string):
-    pass  # TODO: replace this line with your code
+    # TODO: replace this line with your code
+    camel_case = []
+    string = string.split('_')
 
+    for word in string:
+        camel_case.append(f"{word[0].title()}{word[1:].title()}")
+
+    return "".join(camel_case)
+
+print(snake_to_camel("hello_word"))
 
 def longest_word_length(words):
     pass  # TODO: replace this line with your code
